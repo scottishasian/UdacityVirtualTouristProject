26a0c29cd1a958622904f5a8332302bbd00ddc29
Kynan <k.song@blueyonder.co.uk>
2019-01-03 10:27:18 +0000

    Added mapview controller and basic views

---
+++ UdacityVirtualTouristApp/View Controllers/MapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapViewController.swift
+//  UdacityVirtualTouristApp
+//
+//  Created by Kynan Song on 03/01/2019.
+//  Copyright © 2019 Kynan Song. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
